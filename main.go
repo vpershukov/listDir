4a25c870c0f96fe19243d08be2c9c4d47df4eaa4
Vladimir Pershukov <vladimirpershukov@yandex.ru>
2020-01-19 19:05:39 +0300

    first assign

--- main.go
+++ main.go
@@ -6,21 +6,33 @@ import (
 	"io/ioutil"
 	"os"
 	"strconv"
+	"math"
 )
 
 var (
 	d = flag.String("d", ".", "Directory to process")
 	a = flag.Bool("a", false, "Print all info")
+	h = flag.Bool("h", false, "File size converter")
 )
 
-func hrSize(fsize int64) string {
-	return "0KB"
+func hrSize(fsize int) string {
+	if fsize < 1048576 {
+		finalFsize := int(math.Ceil(float64(fsize)/1024))
+		return strconv.Itoa(finalFsize) + "KB"
+	} else {
+		finalFsize := int(math.Ceil(float64(fsize)/(1048576)))
+		return strconv.Itoa(finalFsize) + "MB"
+	}
 }
 
 func printAll(file os.FileInfo) {
 	time := file.ModTime().Format("Jan 06 15:4")
-	fSize := strconv.Itoa(int(file.Size()))
-	fmt.Printf("%s %s %s \n", fSize, time, file.Name())
+	fSize := int(file.Size())
+	if *h {
+		fmt.Printf("%s %s %s \n", hrSize(fSize), time, file.Name())
+	} else {
+		fmt.Printf("%s %s %s \n", fSize, time, file.Name())
+	}
 }
 
 func main() {
